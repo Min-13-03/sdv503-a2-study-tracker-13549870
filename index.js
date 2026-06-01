3cf844dfba5f769f4cebc1571ba898fa88cfef0f
Nazmin Kata <nazminnisha5424@gmail.com>
2026-06-01 12:08:10 +1200

    Add Study Sesssion

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+const sessions = [];
+
+function showMenu() {
+    console.log("\n=== Study Tracker ===");
+    console.log("1. Add Study Session");
+    console.log("2. List Sessions");
+    console.log("3. Show Total Minutes");
+    console.log("4. Exit");
